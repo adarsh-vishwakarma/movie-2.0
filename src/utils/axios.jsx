97aed5974df80649377004fe8474edc898264b30
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-11-12 11:14:16 +0530

    Hide my API Key

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
+import Authorization from "../config";
 
 const instace = axios.create({
     baseURL : "https://api.themoviedb.org/3/",
     headers: {
         accept: 'application/json',
         'content-type': 'application/json',
-        Authorization: ""
+        Authorization: Authorization
       },
 });
 

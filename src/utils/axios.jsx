c662888afdd8693b5cb19b475aa6c1c5e9f8330a
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-11-22 18:05:59 +0530

    d

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -6,7 +6,7 @@ const instace = axios.create({
     headers: {
         accept: 'application/json',
         'content-type': 'application/json',
-        Authorization: Authorization
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGNhY2E1Y2ExZGM3ZDk0NDAyZmVhYjU2ODFjYmU5NiIsIm5iZiI6MTcyNDQ4OTc1MS44MjYyMjcsInN1YiI6IjY2YmYwYmQzZTg1NDMzODhhZTkyZmE4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dGw6zYxRsi-3i7pztdn6iwzXu5emRmKe4vAptEHkN18'
       },
 });
 
